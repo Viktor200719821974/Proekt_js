c470c0ce016998d3a5f5d4a204c6835616f28a20
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-08-02 13:59:04 +0200

    break

---
+++ user.js
@@ -0,0 +1,16 @@
+const mainDiv = document.getElementById('users');
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(value => value.json())
+    .then(users => {
+        let userDiv = document.getElementsByClassName('target')[0];
+        for (const user of users) {
+            console.log(user);
+            let hDiv = document.createElement('h3');
+            let buttonUser = document.createElement('a')
+            hDiv.innerText = `${user.id}. ${user.name}`;
+            buttonUser.innerText = 'Details';
+            buttonUser.href = `user-details.html?users=${JSON.stringify(user)}`;
+            userDiv.appendChild(hDiv);
+            userDiv.appendChild(buttonUser);
+        }
+    });
